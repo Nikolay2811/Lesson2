b316a19c6681d2a9512bfea5dfc142c8751df076
Nikolay <nikola05@list.ru>
2022-06-30 15:58:24 +0300

    Добавил ДЗ

---
+++ ДЗ/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Вdедите трехзpачное число ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+if (numberInt > 99 && numberInt <1000) 
+{
+    Console.WriteLine((numberInt/10)%10);
+} 
+else
+{
+    Console.WriteLine("Сказано же трехзначное, что не понятно? Еще раз, введите ТРЕХЗНАЧНОЕ ЧИСЛО ");
+}
